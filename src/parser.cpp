7666b6f8b67e6febe9d954b4f381371d937c6b06
Ori Mizrahi <ori.mizrahi1000@gmail.com>
2024-11-01 19:28:28 +0200

    Improved functionality?

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,5 +1,4 @@
 #include "parser.h"
-#include <iostream>
 
 int parse_int(int& index, std::string_view& file_content)
 {
@@ -7,21 +6,30 @@ int parse_int(int& index, std::string_view& file_content)
     int length = file_content.length();
 
     int j = index;
-    while(file_content[++j] != 'e' && j < length) num_len++;
-    std::cout << file_content[j] << '\n';
-    for(j = 0; j < num_len; j++) {
-        if(file_content[j] == '-')
-            num *= -1;
+    int had_minus = 0;
+    while(file_content[++j] != 'e' && j < length) {
+        if (file_content[j] == '-')
+            had_minus = 1;
         else
-            num += (file_content[j+(index)] - '0') * static_cast<int>(pow(10,num_len--));
+            num_len++;
     }
+
+    int num_len_cpy = num_len + had_minus;
+    for(j = 0; j < num_len_cpy; j++) {
+        if(file_content[j+index+1] != '-') {
+            num += (file_content[j+index+1] - '0') * static_cast<int>(pow(10,num_len-1));
+            num_len--;
+        }
+    }
+    if(had_minus == 1)
+        num *= -1;
+
     if(index + j <= length)
         index += j;
     else
         index = -1;
 
     return num;
-
 }
 
 int parse_file(std::string_view file_content)
@@ -30,7 +38,7 @@ int parse_file(std::string_view file_content)
     for(int i = 0; i < length; i++) {
         /* found an int */
         if(i < length - 1 && file_content[i] == 'i' && (file_content[i+1] >= '0' && file_content[i+1] <= '9' || file_content[i+1] == '-')) {
-            int num = parse_int(i, file_content);
+            int num = parse_int(++i, file_content);
 
             if(i == -1) // Failed to parse_int?
                 return EXIT_FAILURE;
